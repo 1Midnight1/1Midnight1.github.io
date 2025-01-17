0a6aec5f4b432e42a24bb5d5187d4fda02167e29
Midnight <abdulzulfiqar10@gmail.com>
2025-01-17 02:20:21 -0500

    breakpoints and animations

---
+++ script.js
@@ -0,0 +1,14 @@
+let elementsArray = document.querySelectorAll(".project, .section-header");
+window.addEventListener('scroll', fadeIn ); 
+function fadeIn() {
+    for (var i = 0; i < elementsArray.length; i++) {
+        var elem = elementsArray[i]
+        var distInView = (elem.getBoundingClientRect().top + elem.getBoundingClientRect().bottom)/2 - window.innerHeight + 20;
+        if (distInView < 0) {
+            elem.classList.add("inView");
+        } else {
+            elem.classList.remove("inView");
+        }
+    }
+}
+fadeIn()
